7939ae43482d1c475e6150c5608f41e3d774203b
derrick_kip <derrick.korir@gmail.com>
2018-06-14 11:34:26 +0300

    [Feature #158244317] Add request page [#Finishes 158244317]

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -1,8 +1,21 @@
 const nav_toggler = document.querySelector('.navtoggle');
 const nav_links = document.querySelector('.nav_pull_right');
 
+const acc = document.querySelectorAll('.js_accordion');
+
 nav_toggler.addEventListener('click', show_nav);
 
 function show_nav() {
     nav_links.classList.toggle('nav_show');
+}
+
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', show_hidden);
+}
+
+function show_hidden(event) {
+    const requests = event.target.nextElementSibling;
+
+    requests.classList.toggle('show');
 }
