e674d34f0fd51d3ead5244ce3ba1b86554cae3c9
Derrickkip <derrick.korir@gmail.com>
2018-06-20 09:24:00 +0300

    [FT #158463762] Add app module

---
+++ app/models.py
@@ -0,0 +1,22 @@
+"""
+Data model class declarations
+"""
+class User:
+    """
+    User model class
+    """
+    def __init__(self, first_name, last_name, email, password):
+        pass
+
+class Driver(User):
+    """
+    Driver model class inherits User
+    """
+    pass
+
+class Rides:
+    """
+    Rides class declaration
+    """
+    pass
+
