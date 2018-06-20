adf0cd32cfb6cea87eba1bca1d172c8e4254f678
Derrickkip <derrick.korir@gmail.com>
2018-06-20 09:31:16 +0300

    [Feature #158463762] Add failing tests for User model

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,5 +9,5 @@ def test_user_model(new_user):
     assert new_user.first_name == 'Daudi'
     assert new_user.last_name == 'Jesee'
     assert new_user.email == 'dj@mail.com'
-    assert new_user.hashed_password != 'letmein'
+    assert new_user.hashed_password != 'password'
     assert new_user.car_details is None
