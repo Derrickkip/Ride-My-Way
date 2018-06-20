619d5e26e07484fb12caf40778f6c038ed3971e9
Derrickkip <derrick.korir@gmail.com>
2018-06-20 09:26:46 +0300

    [#Feature #158463762] Add tests package

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+"""
+Fixtures for tests
+"""
+import pytest
+
+from app.models import User
+
+@pytest.fixture(scope='module')
+def new_user():
+    """
+    Create Instance of User class to be used by the module
+    """
+    user = User('Daudi', 'Jesse', 'Dj@mail.com', 'letmein')
+    return user
---
+++ tests/test_models.py
@@ -0,0 +1,13 @@
+"""
+Tests for app models
+"""
+
+def test_user_model(new_user):
+    """
+    test that user can create account
+    """
+    assert new_user.first_name == 'Daudi'
+    assert new_user.last_name == 'Jesee'
+    assert new_user.email == 'dj@mail.com'
+    assert new_user.hashed_password != 'letmein'
+    assert new_user.car_details is None
