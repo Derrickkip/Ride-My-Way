dd08671ff40230827baf6629652e177fb3f9b133
Derrickkip <derrick.korir@gmail.com>
2018-06-21 04:46:36 +0300

    [Chore #158463813] Add failing tests

--- tests/test_ride_endpoint.py
+++ tests/test_ride_endpoint.py
@@ -9,7 +9,6 @@ def test_get_request(test_client):
     """
     response = test_client.get('/api/v1/rides')
     result = json.loads(response.data)
-    print(result)
     assert result['rides'][0]['origin'] == 'Mombasa'
     assert result['rides'][0]['destination'] == 'Nairobi'
     assert result['rides'][0]['travel_date'] == '23th June 2018'
@@ -23,4 +22,18 @@ def test_get_request(test_client):
     assert result['rides'][1]['price'] == 400
     assert result['rides'][1]['requests'] == []
     assert response.status_code == 200
+
+def test_get_single_ride(test_client):
+    """
+    Test request returns correct ride with specified ID
+    """
+    response = test_client.get('/api/v1/rides/2')
+    result = json.loads(response.data)
+    assert response.status_code == 200
+    assert result['rides']['origin'] == 'Kisumu'
+    assert result['rides']['destination'] == 'Lodwar'
+    assert result['rides']['travel_date'] == '25th June 2018'
+    assert result['rides']['time'] == '12:00 am'
+    assert result['rides']['price'] == 400
+    assert result['rides']['requests'] == []
     
