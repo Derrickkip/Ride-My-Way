a510864937f64c0f3727cb7bb1a5b00608d26539
Derrickkip <derrick.korir@gmail.com>
2018-06-24 15:48:30 +0300

    [Chore #158566381] Add test to check routes working

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,5 +8,8 @@ def test_app_created_correctly(test_client):
     '''
     test app created with right configuration
     '''
+    response = test_client.get('ridemyway/api/v1/rides')
+    assert response.status_code == 200
     assert (current_app is None) is False
     assert (current_app.config['TESTING']) is True
+    assert (current_app.config['DEBUG']) is True
