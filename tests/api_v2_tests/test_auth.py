8bcbcf5618973cdb6cb0d4f374c318c40846d004
Derrickkip <derrick.korir@gmail.com>
2018-06-28 03:27:28 +0300

    [chore #158625463] Addsignup tests

--- tests/api_v2_tests/test_auth.py
+++ tests/api_v2_tests/test_auth.py
@@ -1,3 +1,17 @@
 """
 tests for user authentication
 """
+
+import json
+
+def test_signup(test_client):
+    """
+    test that accounts can be created
+    """
+    my_data = {'first_name':'Simon', 'last_name': 'Mbugua', 
+               'email': 'test@email.com', 'password':"testpassword" }
+    
+    response = test_client.post('/auth/signup', data=json.dumps(my_data),
+                                content_type='application/json')
+
+    assert response.status_code == 201
