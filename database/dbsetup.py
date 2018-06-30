12d631aa2ae9cb52544fe644aeb69e0288e39c2f
Derrickkip <derrick.korir@gmail.com>
2018-06-30 12:25:16 +0300

    [Bug #158741820] remove trailing space causing error in creating db

--- database/dbsetup.py
+++ database/dbsetup.py
@@ -36,7 +36,7 @@ def create_tables():
                 request_id SERIAL primary key,
                 user_id int references users(user_id),
                 ride_id int references rides(ride_id),
-                accept_status varchar(80) default 'pending',
+                accept_status varchar(80) default 'pending'
         )
         """
     )
