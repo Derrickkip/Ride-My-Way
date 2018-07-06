077cc8de5be1f9a4159becfd3a374a0e48ed9747
Derrickkip <derrick.korir@gmail.com>
2018-07-06 07:55:06 +0300

    [Chore] Update config file to get settings from environment

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     Common configurations
     """
     DEBUG = True
-    JWT_SECRET_KEY = '\x1b\xa5*G\xca\x88@\xe37\x8e\x8cP\x18\xef\xa3\xc0r\xaa\xf4\x94H3\xc3\xfd'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 class DevelopmentConfig(Config):
     """
@@ -21,14 +21,14 @@ class TestingConfig(Config):
     Testing configurations
     """
     TESTING = True
-    DATABASE = "postgresql://testuser:testuser@localhost/testdb"
+    DATABASE = os.getenv('TEST_DB')
 
 class ProductionConfig(Config):
     """
     Production configurations
     """
     DEBUG = False
-    DATABASE = 'postgres://oyouuwhharkdie:c784d1dc881d562d9720ab3cce810c1dd9bffc4b74f04adf9aa9ccd23946d5f1@ec2-79-125-127-60.eu-west-1.compute.amazonaws.com:5432/d2s6sniq4ne115'
+    DATABASE = os.getenv('PROD_DATABASE')
 
 CONFIG = {
     'development': DevelopmentConfig,
